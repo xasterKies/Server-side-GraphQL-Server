f8671a960ea2f329ce116312cc344e43adf79bd9
xaster-Kies <samueledward40@gmail.com>
2021-10-06 07:01:16 +0100

    Checking for arguements

--- fullstack-graphql-master/api/src/schema.js
+++ fullstack-graphql-master/api/src/schema.js
@@ -4,7 +4,25 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+   type User {
+       id: ID
+       username: String!
+       type: String!
+       img: String!
+       buddies: [Pet]
+   }
+
+   type Pet {
+        id: ID!
+        createdAt: String!
+        name: String!
+        type: String!
+   }
+
+   type Query {
+       pets(input: PetInput): [Pet]!
+       pet(): Pet
+   }
 
 `;
 
